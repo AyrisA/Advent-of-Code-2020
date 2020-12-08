f56da65c66e4154ea35c0ab562ce7b9a847b6273
Ayris Narock <ayris.a.narock@nasa.gov>
2020-12-08 08:04:37 -0500

    Day 7 -- Part 2

--- day07.py
+++ day07.py
@@ -29,13 +29,41 @@ So, in this example, the number of bag colors that can eventually contain at lea
 
 How many bag colors can eventually contain at least one shiny gold bag? (The list of rules is quite long; make sure you get all of it.)
 """
+"""
+--- Part Two ---
+It's getting pretty expensive to fly these days - not because of ticket prices, but because of the ridiculous number of bags you need to buy!
+
+Consider again your shiny gold bag and the rules from the above example:
+
+faded blue bags contain 0 other bags.
+dotted black bags contain 0 other bags.
+vibrant plum bags contain 11 other bags: 5 faded blue bags and 6 dotted black bags.
+dark olive bags contain 7 other bags: 3 faded blue bags and 4 dotted black bags.
+So, a single shiny gold bag must contain 1 dark olive bag (and the 7 bags within it) plus 2 vibrant plum bags (and the 11 bags within each of those): 1 + 1*7 + 2 + 2*11 = 32 bags!
+
+Of course, the actual rules have a small chance of going several levels deeper than this example; be sure to count all of the bags, even if the nesting becomes topologically impractical!
+
+Here's another example:
+
+shiny gold bags contain 2 dark red bags.
+dark red bags contain 2 dark orange bags.
+dark orange bags contain 2 dark yellow bags.
+dark yellow bags contain 2 dark green bags.
+dark green bags contain 2 dark blue bags.
+dark blue bags contain 2 dark violet bags.
+dark violet bags contain no other bags.
+In this example, a single shiny gold bag must contain 126 other bags.
+
+How many individual bags are required inside your single shiny gold bag?
+"""
 
 def process_contents(inside_stuff):
-    bags = []
+    bags = {}
     if inside_stuff != 'no other bags.':
         braw = [x.strip() for x in inside_stuff.split(',')]
         for b in braw:
-            bags.append(' '.join(b.split()[1:-1]))
+            pcs = b.split()
+            bags[' '.join(pcs[1:-1])] = int(pcs[0])
 
     return bags
 
@@ -51,19 +79,26 @@ def find_containers(bmap, bname):
     return containers
 
 
+def find_contents_count(bmap, bname):
+    ccount = 0
+    if bname in bmap:
+        my_contents = bmap[bname]
+        ccount = sum(my_contents.values())
+        for c in my_contents:
+            ccount += my_contents[c] * find_contents_count(bmap, c)
+
+    return ccount
+
+
 with open('./input07.txt', 'r') as f:
     input = [x.strip().split(' bags contain ') for x in f.readlines()]
 
 bagmap = {}
-
 for rule in input:
-    outside = rule[0]
-    inside = process_contents(rule[1])
-    for bag in [outside] + inside:
-        if bag not in bagmap:
-            bagmap[bag] = set()
-
-    bagmap[outside].update(set(inside))
+    bagmap[rule[0]] = process_contents(rule[1])
 
 sg_containers = find_containers(bagmap, 'shiny gold')
 print("{} bag colors can eventually hold a shiny gold bag".format(len(sg_containers)))
+
+sg_contents = find_contents_count(bagmap, 'shiny gold')
+print("A shiny gold bag must contain {} other bags".format(sg_contents))
